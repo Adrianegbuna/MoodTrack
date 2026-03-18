225c42aba471f6d73ca2fffe88a8c1558ce7b400
Adrian Egbuna <ad123eg456@gmail.com>
2026-03-18 14:50:19 +0100

    Update urls.py

--- MoodTrack/urls.py
+++ MoodTrack/urls.py
@@ -33,7 +33,4 @@ urlpatterns = [
           name='password_reset_confirm'),
 
     path('', include('moodtrack_app.urls')), 
-]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
